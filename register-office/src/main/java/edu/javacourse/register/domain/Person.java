09b01140023cdde3a74cce67aa60ad955db60c58
Anton Kovalyov <kovalyov.af7@gmail.com>
2022-05-04 20:53:29 +0700

    Add domain classes

--- register-office/src/main/java/edu/javacourse/register/domain/Person.java
+++ register-office/src/main/java/edu/javacourse/register/domain/Person.java
@@ -5,19 +5,28 @@ package edu.javacourse.register.domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Table(name = "ro_person")
 @Entity
-@Table(name = "person")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "sex", discriminatorType = DiscriminatorType.INTEGER)
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "person_id")
     private Long personId;
     @Column(name = "first_name")
-
     private String firstName;
     @Column(name = "last_name")
-
     private String lastName;
+    @Column(name = "patronymic")
+    private String patronymic;
+    @Column(name = "date_birth")
+    private LocalDate dateOfBirth;
+    @OneToMany(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY, mappedBy = "person")
+    private List<Passport> passports;
 
     public Long getPersonId() {
         return personId;
@@ -43,12 +52,27 @@ public class Person {
         this.lastName = lastName;
     }
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "personId=" + personId +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<Passport> getPassports() {
+        return passports;
+    }
+
+    public void setPassports(List<Passport> passports) {
+        this.passports = passports;
     }
 }
---
+++ register-office/src/main/java/edu/javacourse/register/domain/PersonFemale.java
@@ -0,0 +1,32 @@
+package edu.javacourse.register.domain;
+/*
+ *   Created by Kovalyov Anton 04.05.2022
+ */
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+import java.util.List;
+
+@Entity
+@DiscriminatorValue("1")
+public class PersonFemale extends Person {
+/*    private List<MarriageCertificate> marriageCertificates;
+    private List<BirthCertificate> birthCertificates;
+
+    public List<MarriageCertificate> getMarriageCertificates() {
+        return marriageCertificates;
+    }
+
+    public void setMarriageCertificates(List<MarriageCertificate> marriageCertificates) {
+        this.marriageCertificates = marriageCertificates;
+    }
+
+    public List<BirthCertificate> getBirthCertificates() {
+        return birthCertificates;
+    }
+
+    public void setBirthCertificates(List<BirthCertificate> birthCertificates) {
+        this.birthCertificates = birthCertificates;
+    }*/
+}
