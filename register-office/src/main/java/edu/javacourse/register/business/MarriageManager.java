99a6d1ced23794f42852cb2d8823b94436a56168
Anton Kovalyov <kovalyov.af7@gmail.com>
2022-05-04 20:56:48 +0700

    MarriageManager init

---
+++ register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -0,0 +1,16 @@
+package edu.javacourse.register.business;
+/*
+ *   Created by Kovalyov Anton 04.05.2022
+ */
+
+import edu.javacourse.register.dao.MarriageDao;
+import edu.javacourse.register.view.MarriageRequest;
+import edu.javacourse.register.view.MarriageResponse;
+
+public class MarriageManager {
+    private MarriageDao dao;
+
+    public MarriageResponse findMarriageCertificate(MarriageRequest request) {
+        throw new UnsupportedOperationException("Unsupported operation");
+    }
+}
