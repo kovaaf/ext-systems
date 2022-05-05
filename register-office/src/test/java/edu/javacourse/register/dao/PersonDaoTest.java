1b34fc7349d2d259082d1e0314f80ed856fe791b
Anton Kovalyov <kovalyov.af7@gmail.com>
2022-05-05 23:19:57 +0700

    Add tests for new features

--- register-office/src/test/java/edu/javacourse/register/dao/PersonDaoTest.java
+++ register-office/src/test/java/edu/javacourse/register/dao/PersonDaoTest.java
@@ -1,6 +1,8 @@
 package edu.javacourse.register.dao;
 
 import edu.javacourse.register.domain.Person;
+import edu.javacourse.register.domain.PersonFemale;
+import edu.javacourse.register.domain.PersonMale;
 import org.junit.Test;
 
 import java.util.List;
@@ -19,9 +21,19 @@ public class PersonDaoTest {
         List<Person> persons = dao.findPersons();
 
         persons.forEach(person -> {
-            System.out.println(person.getFirstName());
-            System.out.println(person.getClass().getName());
-            System.out.println(person.getPassports().size());
+            System.out.println("Name: " + person.getFirstName());
+            System.out.println("Class for sex: " + person.getClass().getName());
+            System.out.println("Number of passports: " + person.getPassports().size());
+            System.out.println("Birth certificate: " + person.getBirthCertificate());
+            if (person instanceof PersonMale p) {
+                System.out.println("Number of birth certificates: " + p.getBirthCertificates().size());
+                System.out.println("Number of marriage certificates: " + p.getMarriageCertificates().size());
+            } else {
+                System.out.println("Number of birth certificates: " + ((PersonFemale)person).getBirthCertificates().size());
+                System.out.println("Number of marriage certificates: " + ((PersonFemale)person).getMarriageCertificates().size());
+            }
+            System.out.println();
+            System.out.println();
         });
     }
 }
